a760e87733510393b3e0f377910af8fe7e9b5ae0
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-11 18:37:12 +0300

    feat: implement Google login functionality and enhance login error handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
 import type { Request, Response } from "express";
-import User from "../models/user.model";
 import asyncHandler from "express-async-handler";
+import { OAuth2Client } from "google-auth-library";
 import { errorResponse, successResponse } from "../shared/response";
+import User from "../models/user.model";
+import config from "../config/config";
 
 const registerNewUser = asyncHandler(async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -53,6 +55,14 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
     });
     return;
   }
+  if (!user.password && !user.isGoogleUser) {
+    errorResponse({
+      res,
+      message: "Invalid email or password",
+      statusCode: 400,
+    });
+    return;
+  }
   const isMatch = await user.comparePassword(password);
   if (!isMatch) {
     errorResponse({
@@ -71,4 +81,73 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
   });
 });
 
-export { registerNewUser, loginUser };
+const client = new OAuth2Client(config.google_client_id);
+const googleLogin = asyncHandler(async (req: Request, res: Response) => {
+  const { idToken } = req.body;
+
+  if (!idToken) {
+    errorResponse({
+      res,
+      message: "Google ID token is required",
+      statusCode: 400,
+    });
+    return;
+  }
+
+  let ticket;
+  try {
+    ticket = await client.verifyIdToken({
+      idToken,
+      audience: config.google_client_id,
+    });
+  } catch (error) {
+    errorResponse({
+      res,
+      message: "Invalid Google ID token",
+      statusCode: 400,
+    });
+    return;
+  }
+
+  const payload = ticket.getPayload();
+  if (!payload) {
+    errorResponse({
+      res,
+      message: "Invalid Google ID token payload",
+      statusCode: 400,
+    });
+    return;
+  }
+  // ! profile here is used to get the profile picture of the user
+  // ? Add it to the user model after updating the user schema
+  const { email, name, profile } = payload;
+  console.log("🚀 ~ googleLogin ~ profile:", profile);
+  if (!email || !name) {
+    errorResponse({
+      res,
+      message: "Google ID token must contain email and name",
+      statusCode: 400,
+    });
+    return;
+  }
+
+  let user = await User.findOne({ email: email.toLowerCase() });
+  if (!user) {
+    user = new User({
+      name,
+      email: email.toLowerCase(),
+      isGoogleUser: true,
+    });
+    await user.save();
+  }
+
+  const token = user.generateAuthToken();
+  successResponse({
+    res,
+    data: { token, user: { id: user._id, name: user.name, email: user.email } },
+    message: "User logged in successfully",
+    statusCode: 200,
+  });
+});
+
+export { registerNewUser, loginUser, googleLogin };
