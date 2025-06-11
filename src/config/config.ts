e08c55ccbc91fd6883e3846f5a79136a295e3053
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-11 18:35:09 +0300

    feat: add google_client_id to config interface for Google authentication

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,12 +7,14 @@ interface Config {
   nodeEnv: string;
   mongoURI: string;
   jwt_secret: string;
+  google_client_id?: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
   mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase",
+  google_client_id: process.env.GOOGLE_CLIENT_ID || "",
   jwt_secret: process.env.JWT_SECRET || "defaultsecretkey",
 };
 
