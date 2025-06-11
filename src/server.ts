e1f91950f64476a51c7e9530460185f739a4e9ef
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-11 18:10:18 +0300

    feat: refactor server setup to improve structure and clarity

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,31 @@
-import { Request, Response } from "express";
+import express from "express";
+import AuthRouter from "./routes/auth.route";
 import config from "./config/config";
 import cors from "cors";
 import connectDB from "./db";
-const express = require("express");
+
 const app = express();
+type Request = express.Request;
+type Response = express.Response;
+
 const port = config.port;
 
 const corsOptions: cors.CorsOptions = {
   origin: function (origin, callback) {
-    // Allow any origin to access your server
     callback(null, true);
   },
-  credentials: true, // Allow credentials (cookies) to be included
+  credentials: true,
 };
+
 app.use(express.json());
 app.use(cors(corsOptions));
-app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+
+app.use("/api/auth", AuthRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running in ${config.nodeEnv} mode`);
