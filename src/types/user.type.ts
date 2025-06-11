0ba9d0d7c1faefec976ef78dd7e656c4c751c489
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-11 18:09:49 +0300

    feat: extend IUserModel interface with password comparison and token generation methods

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -4,7 +4,10 @@ import { userSchema } from "../Schemas/user.schema";
 
 export type IUser = z.infer<typeof userSchema>;
 
-export interface IUserModel extends Document, IUser {}
+export interface IUserModel extends Document, IUser {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+  generateAuthToken(): string;
+}
 
 export type UserTypeWithToken = IUser & {
   token: string;
