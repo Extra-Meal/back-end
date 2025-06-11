39814c2c4481e8cabd6b96235240e42874ae646c
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-11 18:37:25 +0300

    feat: add google login route to authentication endpoints

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { loginUser, registerNewUser } from "../controllers/auth.controller";
+import { loginUser, registerNewUser, googleLogin } from "../controllers/auth.controller";
 const router = express.Router();
 
 router.post("/register", registerNewUser);
 router.post("/login", loginUser);
+router.post("/google-login", googleLogin);
 
 export default router;
