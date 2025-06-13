6234bf5dcfabf5e50143941f26e724e449b0d554
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-13 09:56:43 +0300

    fix: update Config interface to include additional properties for client URL and nodemailer

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,8 +7,13 @@ interface Config {
   nodeEnv: string;
   mongoURI: string;
   jwt_secret: string;
+  client_url: string;
   google_client_id?: string;
   google_client_secret?: string;
+  google_refresh_token?: string;
+  nodemailer_email?: string;
+  nodemailer_password?: string;
+  nodemailer_host?: string;
 }
 
 const config: Config = {
@@ -16,8 +21,13 @@ const config: Config = {
   nodeEnv: process.env.NODE_ENV || "development",
   mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase",
   google_client_id: process.env.GOOGLE_CLIENT_ID || "",
-  jwt_secret: process.env.JWT_SECRET || "defaultsecretkey",
   google_client_secret: process.env.GOOGLE_CLIENT_SECRET || "",
+  google_refresh_token: process.env.GOOGLE_REFRESH_TOKEN || "",
+  jwt_secret: process.env.JWT_SECRET || "defaultsecretkey",
+  client_url: process.env.CLIENT_URL || "https://extrameal.netlify.app",
+  nodemailer_email: process.env.NODEMAILER_EMAIL || "",
+  nodemailer_password: process.env.NODEMAILER_PASSWORD || "",
+  nodemailer_host: process.env.NODEMAILER_HOST || "",
 };
 
 export default config;
