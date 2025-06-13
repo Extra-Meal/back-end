81bb7b44fe7ebb6c205a1372fc8f0fb9d2d21723
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-13 22:49:37 +0300

    feat: include MongoDB document ID in IUser type

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -2,7 +2,7 @@ import mongoose, { Document } from "mongoose";
 import { z } from "zod";
 import { userSchema } from "../Schemas/user.schema";
 
-export type IUser = z.infer<typeof userSchema>;
+export type IUser = z.infer<typeof userSchema> & Pick<Document, "_id">;
 
 export interface IUserModel extends Document, IUser {
   comparePassword(candidatePassword: string): Promise<boolean>;
