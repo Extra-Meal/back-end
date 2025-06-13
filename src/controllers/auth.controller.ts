7407b3b5409f2d882249f9971cf630961992bfa1
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-13 06:07:48 +0300

    fix: update googleLogin to use authorization code and retrieve ID token with axios

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { OAuth2Client } from "google-auth-library";
 import { errorResponse, successResponse } from "../shared/response";
 import User from "../models/user.model";
 import config from "../config/config";
+import axios from "axios";
 
 const registerNewUser = asyncHandler(async (req: Request, res: Response) => {
   const { name, email, password } = req.body.data;
@@ -91,12 +92,12 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
 
 const client = new OAuth2Client(config.google_client_id);
 const googleLogin = asyncHandler(async (req: Request, res: Response) => {
-  const { idToken } = req.body.data;
+  const code = req.body.data;
 
-  if (!idToken) {
+  if (!code) {
     errorResponse({
       res,
-      message: "Google ID token is required",
+      message: "Authorization Code is required",
       statusCode: 400,
     });
     return;
@@ -104,6 +105,26 @@ const googleLogin = asyncHandler(async (req: Request, res: Response) => {
 
   let ticket;
   try {
+    const { data } = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      {
+        code,
+        client_id: config.google_client_id,
+        client_secret: config.google_client_secret,
+        redirect_uri: "postmessage", //  used when the code is returned directly to your frontend app (no redirect).
+        grant_type: "authorization_code",
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    const idToken = data.id_token;
+    if (!idToken) {
+      errorResponse({
+        res,
+        message: "ID token not found in the response",
+        statusCode: 400,
+      });
+      return;
+    }
     ticket = await client.verifyIdToken({
       idToken,
       audience: config.google_client_id,
@@ -128,8 +149,7 @@ const googleLogin = asyncHandler(async (req: Request, res: Response) => {
   }
   // ! profile here is used to get the profile picture of the user
   // ? Add it to the user model after updating the user schema
-  const { email, name, profile } = payload;
-  console.log("🚀 ~ googleLogin ~ profile:", profile);
+  const { email, name, picture } = payload;
   if (!email || !name) {
     errorResponse({
       res,
@@ -145,6 +165,7 @@ const googleLogin = asyncHandler(async (req: Request, res: Response) => {
       name,
       email: email.toLowerCase(),
       isGoogleUser: true,
+      isVerified: true,
     });
     await user.save();
   }
