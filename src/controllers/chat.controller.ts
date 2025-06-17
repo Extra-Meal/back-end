32178549ba8b9d448e951b411aa0f85ef91495e4
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-17 19:29:10 +0300

    feat: add aiChatHandler for processing chatbot messages with streaming support

---
+++ src/controllers/chat.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import { generateText, streamText } from "ai";
+import { tools } from "../ai/tools";
+import model, { SYSTEM_PROMPT } from "../ai/ai-model";
+const aiChatHandler = asyncHandler(async (req: Request, res: Response) => {
+  console.log("Received request body message from chatbot");
+  const { messages } = req.body;
+
+  try {
+    const result = streamText({
+      model,
+      system: SYSTEM_PROMPT,
+      messages,
+      tools,
+      maxSteps: 5,
+      toolCallStreaming: true,
+    });
+
+    // If no tool was called, just return the initial result
+    return result.pipeDataStreamToResponse(res);
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+    return;
+  }
+});
+
+export { aiChatHandler };
